06ae44d684de281236d55978cc76bbdc842c6885
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-02 09:16:18 +0530

    Create factor.java

---
+++ CodeForces/Introduction/factor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(b != 0 && a % b == 0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
