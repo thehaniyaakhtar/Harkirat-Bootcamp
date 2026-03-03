45bd523396d264d07bb040f135fee91900939589
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-03 10:18:58 +0530

    Create hollow_rect.java

---
+++ CodeForces/Patters/hollow_rect.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        for(int i = 0; i < a; i++){
+            for(int j = 0; j < b; j++){
+                if(i == 0 || i == a-1 || j == 0 || j == b-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
