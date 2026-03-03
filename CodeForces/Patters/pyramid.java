c5c0d0388dc80f38ab68e42d04c2676d6690f761
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-03 10:26:01 +0530

    Update pyramid.java

--- CodeForces/Patters/pyramid.java
+++ CodeForces/Patters/pyramid.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-
+ 
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
         int a = sc.nextInt();
         
-        for(int i = 0; i <= a; i++){
-                    System.out.println("*".repeat(i));
+        for(int i = 1; i < a; i++){
+                    System.out.print("*".repeat(i));
+                   
         }
+        
     }
 }
