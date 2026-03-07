71a7b7cefd436bd1f926974378b1a8464bbbf009
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-07 16:59:26 +0530

    Update factorial.java

--- CodeForces/Loops/factorial.java
+++ CodeForces/Loops/factorial.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int prod = 1;
+        long prod = 1;
         
         for(int i = 1; i <= a; i++){
             prod = prod*i;
