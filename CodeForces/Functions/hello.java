a1286b3cf14868126558ca1d36a7f554f85e9dc3
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-14 21:20:56 +0530

    Update hello.java

--- CodeForces/Functions/hello.java
+++ CodeForces/Functions/hello.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Main{
+    public static void PrintMessage(int n){
+        for(int i = 0; i < n; i++){
+            System.out.println("I am learning functions");
+        }
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PrintMessage(n);
+    }
+}
