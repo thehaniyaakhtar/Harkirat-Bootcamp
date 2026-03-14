1d98bb3eaa21437a7743ff12d46de8b96b672918
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-14 14:27:01 +0530

    Update hello_cf.java

--- CodeForces/Contest 1/hello_cf.java
+++ CodeForces/Contest 1/hello_cf.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] main){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        for(int i = 1; i <= a; i++){
+            System.out.println("Hello Codeforces " + i);
+            
+        }
+    }
+}
