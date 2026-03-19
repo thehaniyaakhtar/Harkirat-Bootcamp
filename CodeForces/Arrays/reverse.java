a3021f0ef2ff2269a463d086ea104c1bb1f53f35
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-19 19:43:07 +0530

    Update reverse.java

--- CodeForces/Arrays/reverse.java
+++ CodeForces/Arrays/reverse.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+import java.util.Arrays;
 
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int start = 0;
+        int end = n-1;
+        
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
