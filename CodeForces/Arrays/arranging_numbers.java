4a4019a68d1a466a60f07f74c886127b0da8edef
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-20 20:12:32 +0530

    Create arranging_numbers.java

---
+++ CodeForces/Arrays/arranging_numbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        while(a-- > 0){
+            int n = sc.nextInt();
+            
+            for(int i = 1; i <= n; i++){
+                if(i % 2 != 0){
+                    System.out.print(i + " ");
+                }
+            }
+            
+            for(int i = n; i >= 2; i--){
+                if(i % 2 == 0){
+                    System.out.print(i + " ");
+                }
+            }
+        }
+        System.out.println();
+    }
+}
