a7d86f1bba0fa9079c745432756670cee8e94421
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-03-29 11:28:34 +0530

    Update row_order.java

--- CodeForces/2D_Arrays/row_order.java
+++ CodeForces/2D_Arrays/row_order.java
@@ -1,1 +1,23 @@
 
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                sb.append(st.nextToken()).append(" ");
+            }
+        }
+        System.out.print(sb);
+        
+    }
+}
