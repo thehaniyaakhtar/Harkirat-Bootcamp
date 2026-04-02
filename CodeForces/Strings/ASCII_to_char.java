e83b05582a113dedab6fddc0727444e838e39a39
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-04-02 17:53:33 +0530

    Create ASCII_to_char.java

---
+++ CodeForces/Strings/ASCII_to_char.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int ascii = br.readLine().charAt(0);
+        char c = (char) ascii;
+        
+        System.out.println(c);
+    }
+}
