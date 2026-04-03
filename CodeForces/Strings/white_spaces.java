b356f2c21a5822320a2610b1e0406ca29518a065
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-04-03 09:38:03 +0530

    Create white_spaces.java

---
+++ CodeForces/Strings/white_spaces.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = br.readLine();
+
+        String result = s.replace("\\s", "");
+        
+        System.out.println(result);
+        
+    }
+}
