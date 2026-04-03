0a1cca935c350742f434e330f2c543b5780f5f5e
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2026-04-03 21:03:55 +0530

    Create methods.java

---
+++ CodeForces/Strings/methods.java
@@ -0,0 +1,13 @@
+// Basic String Methods
+s.length()
+// length of string
+s.chatAt(i)
+// char at index i
+s.substring(a, b)
+// substring from a to b
+s.substring(a)
+// substring from a to end
+s.equalsIgnoreCase(t)
+// ignore case of t
+s.compareTo(t)
+// lexicographical order
